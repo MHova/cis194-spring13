e2d4cf776f0888e86fbfa869771f06a96c116ca9
Michael Ho <mho@sumall.com>
2017-06-02 00:49:40 -0700

    Most of week 2

---
+++ week02/LogAnalysis.hs
@@ -0,0 +1,32 @@
+{-# OPTIONS_GHC -Wall #-}
+module LogAnalysis where
+import Log
+import Data.List.Split (splitOn)
+
+parseMessage :: String -> LogMessage
+parseMessage =
+    innerParse . splitOn " "
+  where
+    innerParse ("E":severity:timestamp:message) =
+      LogMessage (Error $ read severity) (read timestamp) (unwords message)
+    innerParse ("W":timestamp:message) =
+      LogMessage Warning (read timestamp) (unwords message)
+    innerParse ("I":timestamp:message) =
+      LogMessage Info (read timestamp) (unwords message)
+    innerParse unknown = (Unknown . unwords) unknown
+
+parse :: String -> [LogMessage]
+parse = map parseMessage . lines
+
+insert :: LogMessage -> MessageTree -> MessageTree
+insert (Unknown _) mt = mt
+insert lm Leaf = Node Leaf lm Leaf
+insert lm@(LogMessage _ timestamp _) (Node leftTree node@(LogMessage _ existingTimestamp _) rightTree)
+  | timestamp <= existingTimestamp = Node (insert lm leftTree) node rightTree
+  | otherwise = Node leftTree node (insert lm rightTree)
+insert _ _ = error "wtf there was an unknown node in the tree"
+
+build :: [LogMessage] -> MessageTree
+build = foldl (flip insert) Leaf
+
+inOrder :: MessageTree -> [LogMessage]
