2db91ed0c7457f4e6723529ab1be0b2cb9d6ced5
Michael Ho <mho@sumall.com>
2017-06-03 03:54:01 -0700

    I forgot how much of a pain week 3 was

--- week03/Golf.hs
+++ week03/Golf.hs
@@ -1,5 +1,7 @@
 module Golf where
 
+import Data.List (group, sort)
+
 -- TODO: add comments
 
 skips :: [a] -> [[a]]
@@ -14,5 +16,24 @@ localMaxima l =
   where
     f ((a, b), c) = b > a && b > c
 
+
 histogram :: [Integer] -> String
-histogram = undefined
+histogram l =
+    concat $ reverse (map f [0..m - 1]) ++ ["==========\n0123456789\n"]
+  where
+    -- read from the bottom
+
+    f i = map (!! i) v ++ "\n"
+    -- ^ construct a String from the i-th character in each of the Strings in v. Also
+    --   append a linebreak.
+    v = map (\a -> take m $ replicate a '*' ++ repeat ' ') x
+    -- ^ convert each item in x into astericks strings with enough spaces padded on the
+    --   right to reach size m
+    m = maximum x
+    -- ^ the highest frequency out of all the digits
+    x = map (flip (-) 1 . length) . group . sort $ [0..9] ++ l
+    {- ^ create a 10-item list of frequencies of the digits from 0 to 9. First append
+         the list [0..9] to the input to guarantee that each digit is represented.
+         Then `sort` the list and `group` the items. Convert each resulting sublist to
+         its length - 1 (length - 1 because we tacked on an extra [0..9] at the beginning)
+    -}
