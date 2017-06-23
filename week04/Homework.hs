6c2bf0eb3d71f15c4643a756f3ad791eba51837f
Michael Ho <mho@sumall.com>
2017-06-23 10:49:00 -0700

    foldTree

--- week04/Homework.hs
+++ week04/Homework.hs
@@ -23,3 +23,25 @@ fun2' :: Integer -> Integer
 fun2' = sum . filter even . takeWhile (/= 1) . iterate f
   where
     f n = bool (3 * n + 1) (n `div` 2) (even n)
+
+data Tree a = Leaf
+            | Node Integer (Tree a) a (Tree a)
+  deriving (Show, Eq)
+
+foldTree :: [a] -> Tree a
+foldTree = foldr insertNode Leaf
+
+insertNode :: a -> Tree a -> Tree a
+insertNode newElement Leaf = Node 0 Leaf newElement Leaf
+insertNode newElement (Node currHeight left currElement right)
+  | height left < height right =
+      Node (max (height newLeft) (height right) + 1) newLeft currElement right
+  | otherwise =
+      Node (max (height left) (height newRight) + 1) left currElement newRight
+  where
+    newLeft = insertNode newElement left
+    newRight = insertNode newElement right
+
+height :: Tree a -> Integer
+height Leaf = -1
+height (Node h _ _ _) = h
