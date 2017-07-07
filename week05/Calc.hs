8c08a144f52856db5c187eb435250631b7a38799
Michael Ho <mho@sumall.com>
2017-07-06 19:18:05 -0700

    Exercise 3

--- week05/Calc.hs
+++ week05/Calc.hs
@@ -10,3 +10,16 @@ eval (Mul left right) = eval left * eval right
 
 evalStr :: String -> Maybe Integer
 evalStr = fmap eval . parseExp Lit Add Mul
+
+class Expr a where
+  lit :: Integer -> a
+  add :: a -> a -> a
+  mul :: a -> a -> a
+
+instance Expr ExprT where
+  lit = Lit
+  add = Add
+  mul = Mul
+
+reify :: ExprT -> ExprT
+reify = id
