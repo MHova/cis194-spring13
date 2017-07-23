9a01d9f452af662c94b445bf63c14b8a45d14abf
Michael Ho <mho@sumall.com>
2017-07-23 15:45:06 -0700

    Exercise 4

--- week05/Calc.hs
+++ week05/Calc.hs
@@ -23,3 +23,52 @@ instance Expr ExprT where
 
 reify :: ExprT -> ExprT
 reify = id
+
+testExp :: Expr a => Maybe a
+testExp = parseExp lit add mul "(3 * -4) + 5"
+
+testInteger = testExp :: Maybe Integer
+testBool = testExp :: Maybe Bool
+testMM = testExp :: Maybe MinMax
+testSat = testExp :: Maybe Mod7
+
+instance Expr Integer where
+  lit = id
+  add = (+)
+  mul = (*)
+
+instance Expr Bool where
+  lit n | n <= 0 = False
+        | otherwise = True
+  add = (||)
+  mul = (&&)
+
+newtype MinMax = MinMax Integer
+  deriving (Eq, Show)
+  {- |
+    Alternately we can cheat and have MinMax derive Ord. Because MinMax is just
+    a newtype around Integer, the compiler is smart enough to simply defer to
+    Integer's Ord instance to generate the implementation of MinMax's Ord
+    instance. See below for why this is beneficial.
+  -}
+  -- deriving (Eq, Show, Ord)
+
+instance Expr MinMax where
+  lit = MinMax
+  add (MinMax a) (MinMax b) = MinMax {- or lit -} $ min a b
+  mul (MinMax a) (MinMax b) = MinMax {- or lit -} $ max a b
+  {-|
+    add and mul can be reduced to the below implementations if we cheat and
+    have MinMax derive Ord as well.
+  -}
+  -- add = min
+  -- mul = max
+
+newtype Mod7 = Mod7 Integer deriving (Eq, Show)
+
+instance Expr Mod7 where
+  lit = Mod7 . (`mod` 7)
+  -- probably easier to read in non-point-free style
+  -- lit a = Mod7 (a `mod` 7)
+  add (Mod7 a) (Mod7 b) = lit $ add a b
+  mul (Mod7 a) (Mod7 b) = lit $ mul a b
