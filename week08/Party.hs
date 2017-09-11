834fb9bfcb973a2c5aa250114abd9aee56f5604d
Michael Ho <mho@sumall.com>
2017-09-11 08:08:14 -0700

    Exercise 3 maybe...?

--- week08/Party.hs
+++ week08/Party.hs
@@ -3,6 +3,7 @@
 
 module Party where
 
+import Data.Tree (Tree, flatten)
 import Employee (GuestList(GL), Employee(Emp, empFun))
 
 {- Exercise 1 -}
@@ -31,3 +32,30 @@ moreFun :: GuestList -> GuestList -> GuestList
 moreFun = max
 
 {- Exercise 2 -}
+treeFold :: (a -> b -> b) -> b -> Tree a -> b
+treeFold f acc = foldr f acc . flatten
+
+{- Exercise 3 -}
+
+nextLevel :: Employee -> [(GuestList, GuestList)] -> (GuestList, GuestList)
+nextLevel boss gls = (withThisBoss, withoutThisBoss)
+  where
+    withoutThisBoss :: GuestList
+    withoutThisBoss = mconcat $ map (uncurry moreFun) gls
+
+    withThisBoss :: GuestList
+    withThisBoss = glCons boss withoutThisBoss
+
+    {-
+    withThisBoss :: GuestList
+    withThisBoss = glCons boss . mconcat . map withOrWithoutThisBoss $
+      zip gls subTreesWithThisBoss
+    
+    withOrWithoutThisBoss :: ((GuestList, GuestList), GuestList) -> GuestList
+    withOrWithoutThisBoss ((withSub, withoutSub), withoutSubWithThisBoss)
+      | withSub > withoutSubWithThisBoss = withSub
+      | otherwise = withoutSub
+
+    subTreesWithThisBoss :: [GuestList]
+    subTreesWithThisBoss = map (glCons boss . snd) gls
+    -}
