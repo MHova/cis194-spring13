e5983b7b679fd48dbe14a4a05971f50859618ee3
Michael Ho <mho@sumall.com>
2017-11-07 15:52:46 -0800

    Week 12 exercise 2

--- week12/Risk.hs
+++ week12/Risk.hs
@@ -1,8 +1,10 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE NamedFieldPuns #-}
 
 module Risk where
 
 import Control.Monad.Random
+import Data.List (sort, partition, length)
 
 ------------------------------------------------------------
 -- Die values
@@ -26,3 +28,53 @@ die = getRandom
 type Army = Int
 
 data Battlefield = Battlefield { attackers :: Army, defenders :: Army }
+
+battle :: Battlefield -> Rand StdGen Battlefield
+battle Battlefield{attackers, defenders} =
+    roll attackingUnits >>= \attackRolls ->
+    roll defendingUnits >>= \defenseRolls ->
+    let (aWins, dWins) = partition didAttackerRollHigher $ zip attackRolls defenseRolls
+    in return $ Battlefield (attackers - length dWins) (defenders - length aWins)
+  where
+    attackingUnits :: Int
+    attackingUnits 
+      | attackers >= 3 = 2
+      | attackers == 2 = 1
+      | otherwise = error "wtf can't attack with fewer than 2 units"
+    
+    defendingUnits :: Int
+    defendingUnits
+      | defenders >= 2 = 2
+      | defenders == 1 = 1
+      | otherwise = error "wtf why are we even battling if there are no defenders?"
+
+    roll :: Int -> Rand StdGen [DieValue]
+    roll numDice = reverse . sort <$> sequence (replicate numDice die)
+
+    didAttackerRollHigher :: (DieValue, DieValue) -> Bool
+    didAttackerRollHigher (aRoll, dRoll) = aRoll > dRoll
+
+battle' :: Battlefield -> Rand StdGen Battlefield
+battle' Battlefield{attackers, defenders} = do
+    attackRolls <- roll attackingUnits
+    defenseRolls <- roll defendingUnits
+    let (aWins, dWins) = partition didAttackerRollHigher $ zip attackRolls defenseRolls
+    return $ Battlefield (attackers - length dWins) (defenders - length aWins) 
+  where
+    attackingUnits :: Int
+    attackingUnits 
+      | attackers >= 3 = 2
+      | attackers == 2 = 1
+      | otherwise = error "wtf can't attack with fewer than 2 units"
+    
+    defendingUnits :: Int
+    defendingUnits
+      | defenders >= 2 = 2
+      | defenders == 1 = 1
+      | otherwise = error "wtf why are we even battling if there are no defenders?"
+
+    roll :: Int -> Rand StdGen [DieValue]
+    roll numDice = reverse . sort <$> sequence (replicate numDice die)
+
+    didAttackerRollHigher :: (DieValue, DieValue) -> Bool
+    didAttackerRollHigher (aRoll, dRoll) = aRoll > dRoll
